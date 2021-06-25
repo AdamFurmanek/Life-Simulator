e66fb50e53c6a9516bc0a1ab377cf692625b88f0
Adam Furmanek <adam.furmanek.adios@gmail.com>
2021-06-25 16:03:26 +0200

    Speed, changing human/car

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     public NavMeshAgent agent;
+    bool done = false;
+    public GameObject human, car;
 
     private void Start()
     {
@@ -13,7 +15,37 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (!done)
+        {
             agent.SetDestination(City.Houses[Random.Range(0, City.Houses.Count)].transform.position);
+            done = !done;
+        }
+
+        NavMeshHit navHit;
+        if (NavMesh.SamplePosition(transform.position, out navHit, 1f, NavMesh.AllAreas))
+        {
+            int mask1 = navHit.mask;
+            int index = 0;
+
+            while ((mask1 >>= 1) > 0)
+            {
+                index++;
+            }
+            float areaCost = NavMesh.GetAreaCost(index);
+            agent.speed = 5 / areaCost;
+
+            if(index == 4)
+            {
+                human.SetActive(true);
+                car.SetActive(false);
+            }
+            else if(index == 3)
+            {
+                human.SetActive(false);
+                car.SetActive(true);
+            }
+
+        }
     }
 
 }
